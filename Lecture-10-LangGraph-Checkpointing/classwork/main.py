f0d5f9c89547fbdbfc833407b16513730ae201e5
Richa-Chauhan2905 <chauhanricha2911@gmail.com>
2025-06-18 11:31:03 +0530

    langchain-checkpointing

--- Lecture-10-LangGraph-Checkpointing/classwork/main.py
+++ Lecture-10-LangGraph-Checkpointing/classwork/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from classwork!")
+from graph import create_chat_graph
+from dotenv import load_dotenv
+from langgraph.checkpoint.mongodb import MongoDBSaver
+
+load_dotenv()
+MONGODB_URI = "mongodb://localhost:27017/"
+config = {"configurable": {"thread_id": "20"}}
+
+def init():
+    with MongoDBSaver.from_conn_string(MONGODB_URI) as checkpointer:
+        graph_with_mongo = create_chat_graph(checkpointer=checkpointer)
+    
+        while True:
+            user_input = input("> ")
+            for event in graph_with_mongo.stream({ "messages": [{"role": "user", "content": user_input}] }, config, stream_mode="values"):
+                if "messages" in event:
+                    event["messages"][-1].pretty_print()
 
 
-if __name__ == "__main__":
-    main()
+init()
