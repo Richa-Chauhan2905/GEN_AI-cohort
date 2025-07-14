69d7909c166b870e07c739c8229804242c81c3dd
Richa-Chauhan2905 <chauhanricha2911@gmail.com>
2025-07-14 20:54:27 +0530

    mcp

---
+++ Lecture-12-MCP/mcp-js/index.js
@@ -0,0 +1,17 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
+import { z } from 'zod'
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
+
+const server = new McpServer({
+    name: "My Server",
+    version: "1.0.0"
+})
+
+server.tool("add", { a: z.number(), b: z.number() }, async function({a, b}){
+    const sum = a + b
+    return { content: [{ type: 'text', text: String(sum) }]}
+})
+
+const transport = new StdioServerTransport()
+await server.connect(transport)
+console.log("Server is ready")
